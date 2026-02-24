e01427e7b56be85246fbd9492083d9840d4e62e7
harut <harutinnnn@gmail.com>
2026-02-24 15:34:12 +0400

    First

--- project-management-app/src/pages/auth/Login.tsx
+++ project-management-app/src/pages/auth/Login.tsx
@@ -2,22 +2,38 @@ import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useAuth} from "@/context/AuthContext";
 import {loginRequest} from "@/api/auth.api";
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup"
 
 export const Login = () => {
+
     const {login} = useAuth();
     const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+
+    const loginSchema = Yup.object({
+        loginEmail: Yup.string().email("Invalid email").required("Required"),
+        loginPassword: Yup.string().min(6, "Minimum 6 characters").required("Required"),
+    });
+
+    type LoginFormValues = {
+        loginEmail: string;
+        loginPassword: string;
+    };
+
+
+    const handleLoginSubmit = async (values: LoginFormValues) => {
+
         setError("");
+
+
+        const email = values.loginEmail;
+        const password = values.loginPassword;
 
         try {
             const data = await loginRequest({email, password});
-            console.log(data);
 
             localStorage.setItem("accessToken", data.token);
             localStorage.setItem("refreshToken", data.refreshToken);
@@ -39,36 +55,40 @@ export const Login = () => {
 
 
             <h1 className={"mb-20"}>Sign In</h1>
+            {error.length > 0 && <div className="error-msg">{error}</div>}
+            <Formik
+                initialValues={{loginEmail: "", loginPassword: ""}}
+                validationSchema={loginSchema}
+                onSubmit={handleLoginSubmit}
+            >
+                <Form>
+                    <div className={"input-row"}>
+                        <label htmlFor="email">Email</label>
+                        <Field
+                            type="email"
+                            name="loginEmail"
+                            id="loginEmail"
+                            placeholder="Email"
+                        />
+                        <ErrorMessage name="loginEmail" component="div" className="error-msg"/>
+                    </div>
 
-            <form action="" onSubmit={handleSubmit}>
-                <div className={"input-row"}>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Email" value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
+                    <div className={"input-row"}>
+                        <label htmlFor="loginPassword">Password</label>
+                        <Field
+                            type="password"
+                            id="loginPassword"
+                            placeholder="Password"
+                            name="loginPassword"
+                        />
+                        <ErrorMessage name="loginPassword" component="div" className="error-msg"/>
+                    </div>
 
-                <div className={"input-row"}>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div className={"input-row"}>
-                    <button className={'btn primary'}>Sign in</button>
-                </div>
-            </form>
-
+                    <div className={"input-row"}>
+                        <button className={'btn primary'}>Sign in</button>
+                    </div>
+                </Form>
+            </Formik>
         </div>
 
     )
--- project-management-app/src/pages/auth/Register.tsx
+++ project-management-app/src/pages/auth/Register.tsx
@@ -1,22 +1,47 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useAuth} from "@/context/AuthContext";
-import {loginRequest, registerRequest} from "@/api/auth.api";
+import {registerRequest} from "@/api/auth.api";
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup"
 
 export const Register = () => {
     const {login} = useAuth();
     const navigate = useNavigate();
 
-    const [companyName, setCompanyName] = useState("");
-    const [name, setName] = useState("");
-    const [address, setAddress] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+    const signupSchema = Yup.object({
+        companyName: Yup.string().min(3, "Minimum 3 characters").required("Required"),
+        name: Yup.string().min(3, "Minimum 3 characters").required("Required"),
+        address: Yup.string().min(3, "Minimum 3 characters").required("Required"),
+        email: Yup.string().email("Invalid email").required("Required"),
+        password: Yup.string().min(6, "Minimum 6 characters").required("Required"),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref("password")], "Passwords must match")
+            .required("Required"),
+    });
+
+    type SignupFormValues = {
+        companyName: string;
+        name: string;
+        address: string;
+        email: string;
+        password: string;
+        confirmPassword: string;
+    };
+
+
+    const handleSubmit = async (values: SignupFormValues) => {
+
         setError("");
+
+        const companyName = values.companyName;
+        const name = values.name;
+        const email = values.email;
+        const password = values.password;
+        const address = values.address;
+
 
         try {
             const data = await registerRequest({companyName, name, email, password, address});
@@ -41,71 +66,88 @@ export const Register = () => {
 
             <h1 className={"mb-20"}>Sign Up</h1>
 
-            <form action="" onSubmit={handleSubmit}>
+            {error.length > 0 && <div className="error-msg">{error}</div>}
+            <Formik
+                initialValues={{companyName: "", name: "", email: "", password: "", confirmPassword: "", address: ""}}
+                validationSchema={signupSchema}
+                onSubmit={handleSubmit}
+            >
+                <Form>
 
-                <div className={"input-row"}>
-                    <label htmlFor="companyName">Company name *</label>
-                    <input
-                        type="text"
-                        id="companyName"
-                        placeholder="Company name" value={companyName}
-                        onChange={(e) => setCompanyName(e.target.value)}
-                        required
-                    />
-                </div>
-
-
-                <div className={"input-row"}>
-                    <label htmlFor="name">Your name *</label>
-                    <input
-                        type="text"
-                        id="name"
-                        placeholder="Company name" value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </div>
+                    <div className={"input-row"}>
+                        <label htmlFor="companyName">Company name *</label>
+                        <Field
+                            type="text"
+                            id="companyName"
+                            name="companyName"
+                            placeholder="Company name"
+                        />
+                        <ErrorMessage name="companyName" component="div" className="error-msg"/>
+                    </div>
 
 
-                <div className={"input-row"}>
-                    <label htmlFor="email">Your Email *</label>
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Email" value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div className={"input-row"}>
-                    <label htmlFor="password">Password *</label>
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div className={"input-row"}>
-                    <label htmlFor="address">Company address (optional)</label>
-                    <input
-                        type="text"
-                        id="address"
-                        placeholder="Address" value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                        required
-                    />
-                </div>
+                    <div className={"input-row"}>
+                        <label htmlFor="name">Your name *</label>
+                        <Field
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Company name"
+                        />
+                        <ErrorMessage name="name" component="div" className="error-msg"/>
+                    </div>
 
 
-                <div className={"input-row"}>
-                    <button className={'btn primary'}>Register</button>
-                </div>
-            </form>
+                    <div className={"input-row"}>
+                        <label htmlFor="email">Your Email *</label>
+                        <Field
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                        />
+                        <ErrorMessage name="email" component="div" className="error-msg"/>
+                    </div>
+
+                    <div className={"input-row"}>
+                        <label htmlFor="password">Password *</label>
+                        <Field
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                        />
+                        <ErrorMessage name="password" component="div" className="error-msg"/>
+                    </div>
+
+                    <div className={"input-row"}>
+                        <label htmlFor="confirmPassword">Password *</label>
+                        <Field
+                            type="password"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            placeholder="Password confirmation"
+                        />
+                        <ErrorMessage name="confirmPassword" component="div" className="error-msg"/>
+                    </div>
+
+                    <div className={"input-row"}>
+                        <label htmlFor="address">Company address (optional)</label>
+                        <Field
+                            type="text"
+                            id="address"
+                            name="address"
+                            placeholder="Address"
+                        />
+                        <ErrorMessage name="address" component="div" className="error-msg"/>
+                    </div>
+
+
+                    <div className={"input-row"}>
+                        <button className={'btn primary'}>Register</button>
+                    </div>
+                </Form>
+            </Formik>
 
         </div>
 
