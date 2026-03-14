41058a94a217897ee38aec7d63c95bd7161931ce
harut <harutinnnn@gmail.com>
2026-03-14 23:59:27 +0400

    First

--- project-management-app/src/pages/messages/Messages.tsx
+++ project-management-app/src/pages/messages/Messages.tsx
@@ -1,8 +1,40 @@
-import { Search } from "lucide-react";
+import {Search} from "lucide-react";
 import './Messages.css'
+import {useEffect, useState} from "react";
+import {MemberJoinSkillType} from "@/types/MemberType";
+import {getMembers} from "@/api/members.api";
+import {PageInnerLoader} from "@/components/PageInnerLoder";
 
 const Messages = () => {
 
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const apiUrl: string = import.meta.env.VITE_API_URL || ""
+
+    const [members, setMembers] = useState<MemberJoinSkillType[]>([]);
+    const [activeUser, setactiveUser] = useState<MemberJoinSkillType | null>(null);
+
+
+    useEffect(() => {
+        (async () => {
+            const members: MemberJoinSkillType[] = await getMembers()
+            setMembers(members)
+            setactiveUser(members[0])
+            setLoading(false)
+        })()
+    }, [])
+
+
+    const handleGetMemberConverstaion = async (member: MemberJoinSkillType) => {
+        setactiveUser(member)
+
+
+        //TODO get member conversation
+    }
+
+    if (loading) {
+        return <PageInnerLoader/>;
+    }
 
     return (
         <>
@@ -16,13 +48,49 @@ const Messages = () => {
                     <h4>Conversations</h4>
 
                     <div className={"message-users-search-input input-row relative"}>
-                        <Search className="search-icon" size={22} />
-                        <input type="text" placeholder="Search users..." />
+                        <Search className="search-icon" size={22}/>
+                        <input type="text" placeholder="Search users..." onChange={(e) => {
+
+                            setMembers(prevState => prevState.filter(m => m.user.name.toLowerCase().includes(e.target.value.toLowerCase())))
+                        }}/>
+                    </div>
+
+                    <div className="chat-members-list">
+                        {members && members.map((member: MemberJoinSkillType) => (
+                            <div className={"chat-member " + (activeUser?.user.id === member.user.id ? 'active' : '')}
+                                 key={member.user.id} onClick={async () => {
+                                await handleGetMemberConverstaion(member)
+                            }}>
+
+                                <div className={"message-member-avatar"}>
+                                    <img className={'avatar'}
+                                         src={member.user.avatar ? apiUrl + member.user.avatar : (`/src/assets/avatars/${member.user.gender}.png`)}
+                                         alt="Avatar"/>
+
+                                    <div className={"badge"}></div>
+                                </div>
+                                <div className={"message-member-name"}>
+                                    {member.user.name}
+                                </div>
+                            </div>
+                        ))}
                     </div>
 
                 </div>
 
                 <div className={"message-conversation"}>
+
+                    <div className={"messages-list"}>
+
+                    </div>
+
+                    <div className={"send-message-container"}>
+                        <div className="input-row">
+                            <input type="text"/>
+                            <button type={'button'} className={'btn primary'}></button>
+                        </div>
+                    </div>
+
 
                 </div>
 
