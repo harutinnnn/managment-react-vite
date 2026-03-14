c72881cd043a37763fd5ae110628f27fc6b82a83
harut <harutinnnn@gmail.com>
2026-03-14 15:27:10 +0400

    First

--- project-management-app/src/pages/Messages.tsx
+++ project-management-app/src/pages/Messages.tsx
@@ -1,10 +1,5 @@
 
-import {useState} from "react";
-import {DeleteConfirmation} from "@/components/DeleteConfirmation";
-
 const Messages = () => {
-    const [isConfirmOpen, setIsConfirmOpen] = useState(false);
-
 
 
     return (
@@ -13,40 +8,6 @@ const Messages = () => {
                 <h1>Messages</h1>
             </div>
 
-            <div
-                style={{
-                    maxWidth: "520px",
-                    backgroundColor: "var(--white)",
-                    borderRadius: "12px",
-                    padding: "20px",
-                    boxShadow: "0 0 3px rgba(195, 195, 195, 0.75)",
-                }}
-            >
-
-
-                <div style={{position: "relative", display: "inline-block"}}>
-                    <button
-                        className={"btn sm danger rounded bordered"}
-                        onClick={() => {
-                            setIsConfirmOpen(true);
-                        }}
-                        aria-expanded={isConfirmOpen}
-                        aria-haspopup="dialog"
-                    >
-                        Delete conversation
-                    </button>
-
-                    {isConfirmOpen && (
-                        <DeleteConfirmation
-                            message={"Are you sure"}
-                            onCancel={() => setIsConfirmOpen(false)}
-                            onConfirm={() => setIsConfirmOpen(false)}
-                        />
-                    )}
-                </div>
-
-
-            </div>
         </>
     );
 };
